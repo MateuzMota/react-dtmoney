b66e5a4bcd71bffebfa9959325003b1174211653
Mateus Mota <mateus@marketmedia.com.br>
2022-01-16 21:38:00 -0300

    add and cofig new transaction modal

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,30 @@ import { Header } from "./components/Header";
 
 import { GlobalStyle } from "./styles/global";
 
+import { useState } from "react";
+import { NewTransactionModal } from "./components/NewTransactionModal";
+
 export function App() {
+  const [ isNewTransactionModalOpen, setIsNewTransactionModalOpen ] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+      setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+      setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
       <Dashboard />
-      
+
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
+
       <GlobalStyle />
     </>
   );
